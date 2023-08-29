96e9c8be10b269d939601fd55def8b52e9096937
Constanzafl <florioconstanza@gmail.com>
2023-08-28 21:50:49 -0300

    j

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title('Introduciendonos al mundo de las criptomonedas')
+
+st.markdown('***')
+st.markdown('##hola')
+st.write('texto de ejemplo')
+
+st.sidebar.markdown('panel izquierdo')
