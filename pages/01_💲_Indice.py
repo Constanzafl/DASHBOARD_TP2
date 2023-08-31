fdc3f95545d7f1133094419c92c312801831448d
Constanzafl <florioconstanza@gmail.com>
2023-08-30 21:33:04 -0300

    o

---
+++ pages/01_💲_Indice.py
@@ -0,0 +1,17 @@
+import streamlit as st 
+
+st.title('Indice de navegacion en la APP')
+
+st.markdown('*****')
+st.markdown('**✔️Generalidades**')
+st.text('En este sector vas a encontrar el dataframe que obtuvimos durante la conexión con la API de coingeko, y algunos valores básicos que nos parecen escenciales para tener un primer acercamiento al tema.')
+
+st.markdown('**✔️KPI´s**')
+st.markdown('⭐Historico')
+st.text('')
+st.markdown('⭐ RSI y MM')
+st.text('')
+st.markdown('⭐ ROI')
+st.text('')
+st.markdown('**✔️DYOR**')
+st.text('Último pero no menor, un cierre y algunas conclusiones.')
